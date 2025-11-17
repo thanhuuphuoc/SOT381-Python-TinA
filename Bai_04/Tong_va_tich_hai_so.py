98e81fc107cce455b74e976d48f5918b91a27891
thanhuuphuoc <thanhuuphuoc.nt@gmail.com>
2025-11-17 07:18:06 +0700

    new files for coding

---
+++ Bai_04/Tong_va_tich_hai_so.py
@@ -0,0 +1,7 @@
+print ("Phép tính tổng và tích hai số")
+so_1 = float(input("Nhập số thứ nhất: "))
+so_2 = float(input("Nhập số thứ hai: "))
+kq_tong = so_1 + so_2
+kq_tich = so_1 * so_2
+print ("Tổng hai số là", kq_tong)
+print ("Tích hai số là", kq_tich)
