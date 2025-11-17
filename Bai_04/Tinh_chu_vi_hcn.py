502afb84fbc0fc57d7a1113aa7c710aa1a6d3012
thanhuuphuoc <thanhuuphuoc.nt@gmail.com>
2025-11-17 07:29:39 +0700

    Create Tinh_chu_vi_hcn.py

---
+++ Bai_04/Tinh_chu_vi_hcn.py
@@ -0,0 +1,8 @@
+print ("Chương trình tính chu vi hình chữ nhật")
+ngan = float(input("Nhập độ dài cạnh ngắn: "))
+dai = float(input("Nhập độ dài cạnh dài: "))
+chu_vi = float 
+chu_vi = 2 * (ngan + dai)
+if ngan < 0 or dai < 0:
+    print ("Hình chữ nhật này không hợp lệ")
+else: print ("Chu vi hinh chữ nhật là: ", chu_vi) 
