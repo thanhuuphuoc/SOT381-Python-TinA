b41fce89bcc9a0b9c3778efee93a49e28a78bbf1
thanhuuphuoc <thanhuuphuoc.nt@gmail.com>
2025-12-15 13:59:40 +0700

    bai tap

--- New folder/bai_tap.py
+++
@@ -1,4 +0,0 @@
-a = float(input("Nhập độ dài HCN: "))
-b = float(input("Nhập độ rộng HCN: "))
-print("Chu vi HCN là:", (a+b)*2)
-print("Diện tích HCN là:", a*b)
---
+++ bai_thuc_hanh/bai_tap.py
@@ -0,0 +1,6 @@
+a = float(input("Nhập độ dài HCN: "))
+b = float(input("Nhập độ rộng HCN: "))
+chu_vi = (a+b)*2
+dien_tich = a*b
+print("Chu vi HCN là:", chu_vi)
+print("Diện tích HCN là:", dien_tich)
